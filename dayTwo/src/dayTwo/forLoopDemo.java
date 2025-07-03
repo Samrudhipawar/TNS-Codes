c5803b1c89b81433d2eb2e4bd2433a63dbe18240
HP <HP@SAMS-HP.mshome.net>
2025-07-03 23:14:31 +0530

    Initial Commit.

---
+++ dayTwo/src/dayTwo/forLoopDemo.java
@@ -0,0 +1,11 @@
+package dayTwo;
+
+public class forLoopDemo {
+	public static void main(String[] args)
+	{
+		for(int i=100; i>=1; --i)
+		{
+			System.out.println("Value of i : "+ i );
+		}
+	}
+}
---
+++ dayTwo/src/dayTwo/ifDemo.java
@@ -0,0 +1,18 @@
+package dayTwo;
+
+public class ifDemo {
+	public static void main(String[] args)
+	{
+		int a = 20;
+		int b = 10;
+		
+		if(a>b)
+		{
+			System.out.println("A is greater number");
+		}
+		else
+		{
+			System.out.println("B is greater number");
+		}
+	}
+}
