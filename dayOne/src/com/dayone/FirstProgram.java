06e09de1e94fd95a29f6dc5b5033bfb2ad9eec75
HP <HP@SAMS-HP.mshome.net>
2025-07-03 23:13:02 +0530

    Initial Commit.

---
+++ dayOne/src/com/dayone/FirstProgram.java
@@ -0,0 +1,18 @@
+package com.dayone;
+
+public class FirstProgram {
+	public static void main(String[] args)
+	{
+		//declaration
+		int a;
+		
+		a=10;
+	
+		System.out.println(a);
+		
+		short s = 23;
+		
+		System.out.println(s);
+	}
+
+}
---
+++ dayOne/src/com/dayone/datatypes/PrimitiveDatatypeDemo.java
@@ -0,0 +1,18 @@
+package com.dayone.datatypes;
+
+public class PrimitiveDatatypeDemo {
+	public static void main(String[] args)
+	{
+		
+	char c ='a';
+	System.out.println(c);
+	
+	long l = 9175908507788664L;
+	System.out.println(l);
+	
+	float f = 24547.67f;
+	System.out.println(f);
+	
+	}
+	
+}
