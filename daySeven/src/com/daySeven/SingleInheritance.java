462b30967d0fef6dc86246916d1266fc98193414
HP <HP@SAMS-HP>
2025-07-08 19:38:04 +0530

    Initial Commit.

---
+++ daySeven/src/com/daySeven/SingleInheritance.java
@@ -0,0 +1,13 @@
+package com.daySeven;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Citizen C1 = new Citizen("Shreya",726354377,"pune",765462547);
+		C1 = new Student("shruti",74527627,"mumbai",76345284,23,"jspm");
+		System.out.println(C1);
+
+	}
+
+}
